bf13148d62df66957740fe925f599bfe48d36618
Karl Fischer <karl@obsidian.co.za>
2019-03-11 13:35:59 +0200

    Bumped version to 6.8.0

--- plan.sh
+++ plan.sh
@@ -1,11 +1,11 @@
 pkg_name=atlassian-bamboo
 pkg_origin=kmf
-pkg_version="6.7.2"
+pkg_version="6.8.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_source="https://product-downloads.atlassian.com/software/bamboo/downloads/${pkg_name}-${pkg_version}.tar.gz"
 pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="7b58fd62f1662672e0fb1c6528c8a7815b3276cb859b8a842f88dee90fd1780b"
+pkg_shasum="b2b2a6825a84f296b6cd24b1cb1b3b0109218558d482cf1e846e4ef6a458a2a9"
 pkg_exports=(
   [bamboo_port]=listening_port
 )
